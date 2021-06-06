37144c45eed5e668aa2f023bc3bc6e6d6af6d860
davidmedero <david.gregory.medero@gmail.com>
2021-06-06 01:45:47 -0400

    added dice roll functionality to player 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Click to roll dice. Accumulate points based on the roll number, then click Hold to keep the points. If you roll a One during your turn, you will lose the points you accumulated in that specific turn (you still keep the points from the last turn).
+
+First player to reach 100 points wins the game!!!
--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+// Selecting elements
+const score0E1 = document.querySelector("#score--0");
+const score1E1 = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Starting conditions
+score0E1.textContent = 0;
+score1E1.textContent = 0;
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Swicth to next player
+  }
+});
